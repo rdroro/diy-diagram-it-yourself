f5d41665acdba697c32610c7c94d28389420fa7c
Romain Dubos <romain.dubos@gmail.com>
2014-01-06 22:44:54 +0100

    * add control on input file in diy.py. * add error-code.md

--- bin/diy.py
+++ bin/diy.py
@@ -5,28 +5,50 @@ main program to transform DIY language in diagramm
 
 """
 
-import sys, getopt
+import sys
+import getopt
+from os import path
 from parser import Parser
+
 
 version = '0.0.1'
 
-def usage ():
+
+def usage():
 	print '================'
 	print 'Usage: diy tool to transform DIY language in diagram'
-	print 'diy options:'
-	print '\t-h: Display this help'
-	print '\t-v: print diy version'
-	print '\t-o: path to output svg file'
+	print 'diy [path_to_diy_file] options:'
+	print '\t-h, --help: Display this help'
+	print '\t-v, --version: print diy version'
+	print '\t-o, --output: path to output svg file'
+	print '\t-i, --input: path to input diy file'
 	print ''
 	return
 
-def main ():
+
+def start(input_file):
+	if not path.exists(input_file):
+		print '[ERROR] file: '+input_file+' does not exist'
+		usage()
+		sys.exit(110)
+	print "gg"
+	json = open(input_file)
+	parser = Parser(json)
+	parser.parse()
+	json.close()
+
+
+def main():
 	argv = sys.argv
-	# first argument must be input file
 	argv = argv[1:]
+	if len(argv) < 1:
+		print '[ERROR] diy needs one parameter at least'
+		usage()
+		sys.exit(100)
+
 
 	try:
-		opts, args = getopt.getopt(argv, "hi:vo:", ['help', 'version', 'output', 'input'])
+		opts, args = getopt.getopt(argv, ":hi:vo:", ['help', 'version', 'output', 'input'])
 	except getopt.GetoptError as err:
 		print ''
 		print err
@@ -39,13 +61,12 @@ def main ():
 		elif opt in ('-v', '--version'):
 			print 'diy version '+version
 		elif opt in ('-i', '--input'):
-			json = open(arg)
-			parser = Parser(json)
-			parser.parse()
-			json.close()
-		
+			start(arg)		
 		else:
-			usage()
+			# first argument must be input file
+			# @todo
+			print "Shame ..."
+
 
 if __name__ == "__main__":
     main()
---
+++ doc/error-code.md
@@ -0,0 +1,10 @@
+Error code
+==========
+
+1xx code
+--------
+
+1xx indicates errors in diy main script
+
+100: Number of parameter < 1
+110: first argument (without option) must be a valid path to diy file
