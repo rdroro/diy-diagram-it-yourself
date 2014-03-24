f60d39579dcf2849b5c7e5e2a2324c50f3b89cae
Romain Dubos <romain.dubos@gmail.com>
2014-03-24 21:13:35 +0100

    Source code in bin/ respect pep8 now

--- bin/parser.py
+++ bin/parser.py
@@ -2,59 +2,55 @@ import json
 import re
 import exception
 
+
 class Parser:
-	"""
-	Read DIY language and transform it in JSON
-	"""
+    """
+    Read DIY language and transform it in JSON
+    """
 
-	@staticmethod
-	def parse (file):
-		""""
-		Parse file - a file object - to transform DIY language to JSONArray
+    @staticmethod
+    def parse(file):
+        """"
+        Parse file - a file object - to transform DIY language to JSONArray
 
-		Args:
-			file: file object containing DIY language
-		Returns:
-			the parsed input. JSONArray
-		Raises:
-			NotJSONException if input is not valid JSON input
+        Args:
+            file: file object containing DIY language
+        Returns:
+            the parsed input. JSONArray
+        Raises:
+            NotJSONException if input is not valid JSON input
 
-		"""
+        """
 
-		jsoned = []
+        jsoned = []
 
-		reElements = re.compile("(\w*)\((.*?)\)", re.S)
-		reBlank = re.compile('\s*')
+        reElements = re.compile("(\w*)\((.*?)\)", re.S)
+        reBlank = re.compile('\s*')
 
-		# get only code between tag @diy: :yid@
-		# get each element
-		elements = reElements.findall(file)
-		if len(elements) == 0:
-			raise exception.NoElementFoundExeception()
-		for element in elements:
-			jsonElement = {}
-			jsonElement['type'] = element[0]
-			attributes = element[1].split(";")
-			for couples in attributes:
-				couple = couples.split(":")
-				couple[0] = reBlank.sub("", couple[0].__str__())
-				couple[1] = couple[1].__str__().strip()
-				jsonElement[couple[0].__str__()] = couple[1].__str__()
-			jsoned.append(jsonElement)
+        # get only code between tag @diy: :yid@
+        # get each element
+        elements = reElements.findall(file)
+        if len(elements) == 0:
+            raise exception.NoElementFoundExeception()
+        for element in elements:
+            jsonElement = {}
+            jsonElement['type'] = element[0]
+            attributes = element[1].split(";")
+            for couples in attributes:
+                couple = couples.split(":")
+                couple[0] = reBlank.sub("", couple[0].__str__())
+                couple[1] = couple[1].__str__().strip()
+                jsonElement[couple[0].__str__()] = couple[1].__str__()
+            jsoned.append(jsonElement)
 
+        return jsoned
 
+    @staticmethod
+    def load(file):
+        jsoned = ""
+        try:
+            jsoned = json.loads(file)
+        except Exception as e:
+            raise exception.NotJSONException("")
 
-		return jsoned
-
-	@staticmethod
-	def load(file):
-		jsoned = ""
-		try:
-			jsoned = json.loads(file)
-		except Exception as e:
-			raise exception.NotJSONException("")
-
-		return jsoned
-
-		
-
+        return jsoned
--- bin/svgRender.py
+++ bin/svgRender.py
@@ -1,107 +1,98 @@
 import re
 
+
 class SvgRender:
+    """
+    SvgRender is a class for write SVG element in output
+    """
 
-	"""
-	SvgRender is a class for write SVG element in output
-	"""
+    def __init__(self, lib):
+        """
+        Constructor
 
-	
-	def __init__(self, lib):
-		"""
-		Constructor
+        lib -- string, the path to svg element containing *.svg
 
-		lib -- string, the path to svg element containing *.svg
+        """
+        self.libPath = lib
+        self.svgString = ""
+        self.hasHeader = False
+        self.hasFooter = False
 
-		"""
-		self.libPath = lib
-		self.svgString = ""
-		self.hasHeader = False
-		self.hasFooter = False
+    def addElement(self, element):
+        """
+        Transform json element to svg element. Replace all {{KEY}} value by {{VALUE}}
 
+        element -- dictionnary
 
-	def addElement(self, element):
-		"""
-		Transform json element to svg element. Replace all {{KEY}} value by {{VALUE}}
+        """
+        eltFile = open(self.libPath+'/svg/'+element['type']+'.svg')
+        elt = eltFile.read()
+        eltFile.close
+        for key, value in element.iteritems():
+            if (key != 'type'):
+                elt = elt.replace('{{'+key.__str__()+'}}', value.__str__().decode("utf-8"))
+        self.svgString += "\n"+elt
 
-		element -- dictionnary
+    def prependFragment(self, svgElement):
+        """
+        Simple add svg fragment to the beginning of document
 
-		"""
-		eltFile = open(self.libPath+'/svg/'+element['type']+'.svg')
-		elt = eltFile.read()
-		eltFile.close
-		for key, value in element.iteritems():
-			if (key != 'type'):
-				elt = elt.replace('{{'+key.__str__()+'}}', value.__str__().decode("utf-8"))
-		self.svgString += "\n"+elt
+        svgElement -- string svg element(s)
 
-	
-	def prependFragment(self, svgElement):
-		"""
-		Simple add svg fragment to the beginning of document
+        """
+        if self.hasHeader:
+            # Raise Exception
+            print "[ERROR] Impossible to add fragment. Header is already defined"
+            return
+        self.svgString = "\n"+svgElement+self.svgString
 
-		svgElement -- string svg element(s)
-		
-		"""
-		if self.hasHeader:
-			# Raise Exception
-			print "[ERROR] Impossible to add fragment. Header is already defined"
-			return
-		self.svgString = "\n"+svgElement+self.svgString
+    def getHeader(self):
+        """
+        Return svg header saved in self.libPath/svg/partials/header.tpl
+        """
+        header = open(self.libPath+'/svg/partials/header.tpl')
+        headerStr = header.read()
+        header.close()
+        return headerStr
 
-	
-	def getHeader(self):
-		"""
-		Return svg header saved in self.libPath/svg/partials/header.tpl
-		"""
-		header = open(self.libPath+'/svg/partials/header.tpl')
-		headerStr = header.read()
-		header.close()
-		return headerStr
+    def getFooter(self):
+        """
+        Return svg end tag: \n</svg>
+        """
+        return "\n</svg>"
 
+    def writeHeader(self, size):
+        """
+        Write svg header (in LIB/templates/default/svg/partials/header.tpl)
+        into Public variable self.svgString
+        """
+        headerStr = self.getHeader()
+        headerStr = headerStr.replace('{{x}}', size['x'].__str__())
+        headerStr = headerStr.replace('{{y}}', size['y'].__str__())
+        self.svgString = headerStr+self.svgString
+        self.hasHeader = True
+        return
 
-	def getFooter(self):
-		"""
-		Return svg end tag: \n</svg>
-		"""
-		return "\n</svg>"
+    def writeFooter(self):
+        """
+        Write </svg> tag into Public variable self.svgString
+        """
+        self.svgString += self.getFooter()
+        self.hasFooter = True
+        return
 
+    def __str__(self):
+        """
+        Overwrite __str__ to display a SVG document with header and footer
+        If header and footer are not set, __str__ add them
+        """
+        svgDocument = ""
+        if not self.hasHeader:
+            svgDocument = self.getHeader()
 
-	
-	def writeHeader(self, size):
-		"""
-		Write svg header (in LIB/templates/default/svg/partials/header.tpl) 
-		into Public variable self.svgString
-		"""
-		headerStr = self.getHeader()
-		headerStr = headerStr.replace('{{x}}', size['x'].__str__())
-		headerStr = headerStr.replace('{{y}}', size['y'].__str__())
-		self.svgString = headerStr+self.svgString
-		self.hasHeader = True
-		return
+        svgDocument += self.svgString
 
-	
-	def writeFooter(self):
-		"""
-		Write </svg> tag into Public variable self.svgString
-		"""
-		self.svgString += self.getFooter()
-		self.hasFooter = True
-		return
+        if not self.hasFooter:
+            svgDocument += self.getFooter()
 
-	
-	def __str__(self):
-		"""
-		Overwrite __str__ to display a SVG document with header and footer
-		If header and footer are not set, __str__ add them
-		"""
-		svgDocument = ""
-		if not self.hasHeader:
-			svgDocument = self.getHeader()
-
-		svgDocument += self.svgString
-
-		if not self.hasFooter:
-			svgDocument += self.getFooter()
-
-		return svgDocument
+        return svgDocument
