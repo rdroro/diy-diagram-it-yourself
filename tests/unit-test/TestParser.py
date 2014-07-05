c3e2f7ba7de3c95163aad35bd0fade774c09fafd
Romain Dubos <romain.dubos@gmail.com>
2014-07-05 23:04:54 +0200

    Add unit test for Parser.parse_markdown()
    demo.diy containing same content as demo.json but with
    markdown notation. Tests for parse_markdown and parse_json
    are almost the same.
    
    	modified:   tests/unit-test/TestParser.py
    	new file:   tests/unit-test/samples/demo.diy
    	modified:   tests/unit-test/samples/demo.json

--- tests/unit-test/TestParser.py
+++ tests/unit-test/TestParser.py
@@ -16,12 +16,15 @@ class TestParser(unittest.TestCase):
     def setUp(self):
         print '== Test Parser =='
         jsonfile = codecs.open("unit-test/samples/demo.json",  'r', 'utf-8')
-        self.jsonstr = jsonfile.read().encode("utf8")
+        self.jsonstr = jsonfile.read()
         jsonfile.close()
-        self.markdownstr = ''
+        diyfile = codecs.open("unit-test/samples/demo.diy", 'r', 'utf8')
+        self.markdownstr = diyfile.read()
+        diyfile.close()
 
     def runTest(self):
         self.parse_json_test()
+        self.parse_markdown_test()
 
     def parse_json_test(self):
         # Check the retun type
@@ -37,12 +40,27 @@ class TestParser(unittest.TestCase):
         self.assertEqual(jsonList[0]["type"], "box".decode("utf8"))
         self.assertEqual(jsonList[1]["name"], "Here We go #MFB".decode("utf8"))
         self.assertEqual(jsonList[1]["type"], "circle".decode("utf8"))
-        self.assertEqual(jsonList[1]["position"], "2, 0".decode("utf8"))
+        self.assertEqual(jsonList[1]["position"], "2,0".decode("utf8"))
 
         # If inputstr is not in json format -> Raises exception.NotJSONException
         jsonstr = '["box": name, "type": Without quotes'
         self.assertRaises(exception.NotJSONException, Parser.parse_json, jsonstr)
 
+    def parse_markdown_test(self):
+        # Check the retun type
+        jsonList = Parser.parse_markdown(self.markdownstr)
+        # Must be a list
+        self.assertIsInstance(jsonList, list)
+        # Each elements must be dic
+        for dic in jsonList:
+            self.assertIsInstance(dic, dict)
+
+        # Test values. All variables are store as unicode. decode() returns unicode
+        self.assertEqual(jsonList[0]["name"], "Démo".decode("utf8"))
+        self.assertEqual(jsonList[0]["type"], "box".decode("utf8"))
+        self.assertEqual(jsonList[1]["name"], "Here We go #MFB".decode("utf8"))
+        self.assertEqual(jsonList[1]["type"], "circle".decode("utf8"))
+        self.assertEqual(jsonList[1]["position"], "2,0".decode("utf8"))
 
 if __name__ == '__main__':
     unittest.main()
